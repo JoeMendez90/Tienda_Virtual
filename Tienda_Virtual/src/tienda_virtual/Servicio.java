18ed6ef0b9e7e5909aa47b6d9955eb82b6f5d756
Diego Bulla <dibullap@unal.edu.co>
2020-02-07 19:06:20 -0500

    pequeño avance, infimo XD

--- Tienda_Virtual/src/tienda_virtual/Servicio.java
+++ Tienda_Virtual/src/tienda_virtual/Servicio.java
@@ -7,81 +7,12 @@
 package tienda_virtual;
 
 public class Servicio {
-    /**
-     * Tstandar: el tiempo estandar que tarda el servicio en cumplirse
-     * name: nombre del servicio
-     * cumplido: determina si ya se cumplió el servicio
-     * seller: el que ofrece el servicio
-     * valor: el precio: 
-    */
-    private int Tstandar;
-    private int valor;
-    private String name;
-    private String seller;
-    private String desc;
-    private boolean cumplido;
-
-    public Servicio(int Tstandar, int valor, String name) {
-        setTstandar(Tstandar);
-        setValor(valor);
-        setName(name);
-    }
-    
-    public Servicio(int Tstandar, int valor, String name, String seller){
-        this(Tstandar, valor, name);
-        setSeller(seller);
-    }
-    public Servicio(int Tstandar, int valor, String name, String seller, String desc){
-        this(Tstandar, valor, name, seller);
-        setDesc(desc);
-    }
-    
-    public int getTstandar() {
-        return Tstandar;
-    }
-
-    public void setTstandar(int Tstandar) {
-        this.Tstandar = Tstandar;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSeller() {
-        return seller;
-    }
-
-    public void setSeller(String seller) {
-        this.seller = seller;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-    
-    public boolean isCumplido() {
-        return cumplido;
-    }
-
-    public void setCumplido(boolean cumplido) {
-        this.cumplido = cumplido;
-    }
+    int tiempoMax;
+    int valor;
+    int comision;
+    String nombre;
+    String vendedor;
+    String descripcion;
+    Producto product;
     
 }
