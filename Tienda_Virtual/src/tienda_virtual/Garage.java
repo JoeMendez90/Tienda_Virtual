439346ced686af386c32e665106dfa2e8796c3c5
Diego Bulla <dibullap@unal.edu.co>
2020-02-11 21:02:23 -0500

    Enserio lo siento el commit anterior estaba mal hecho

--- Tienda_Virtual/src/tienda_virtual/Garage.java
+++ Tienda_Virtual/src/tienda_virtual/Garage.java
@@ -12,27 +12,30 @@ package tienda_virtual;
 public class Garage {
     
     public DinamicArray<String> names;
-    private DinamicArray<GuardCarr> garage;
+    //private DinamicArray<GuardCarr> garage;
+    private Hash_Map<String, GuardCarr> garage;
 
     public Garage() {
         names = new DinamicArray<>();
-        garage = new DinamicArray<>();
+        garage = new Hash_Map<>();
     }
 
-    public Garage(DinamicArray<GuardCarr> garage) {
+    public Garage(Hash_Map<String, GuardCarr> garage) {
         this.garage = garage;
         names = new DinamicArray<>();
-        for (int i = 0; i < garage.tam; i++) {
-            names.addBack(garage.get(i).getString());
+        for (int i = 0; i < garage.size(); i++) {
+            names.addBack(garage.elements.get(i).getKey());
+            //names.addBack(garage.get(i).getString());
         }
     }
     
     public void nuevo_carrito(Carrito name, String Nombre){
-        garage.addBack(new GuardCarr(name, Nombre));
+        garage.put(Nombre, new GuardCarr(name,Nombre));
+        //garage.addBack(new GuardCarr(name, Nombre));
         names.addBack(Nombre);
     }
 
-    public DinamicArray<GuardCarr> getGarage() {
+    public Hash_Map<String, GuardCarr> getGarage() {
         return garage;
     }
     
